84abc0ebc5f2036e80239569cd97901898779865
InzaSanam08 <inzasanam7@gmail.com>
2025-01-27 20:17:54 +0500

    python Notes

--- python_work/Data_Types/dataTypes.py
+++ python_work/Data_Types/dataTypes.py
@@ -40,4 +40,7 @@ a = [1,2,3,4,5,6,7,8,9,10]
 x = ('its','Time','To','Achieve','Goals')
 a = (1,2,3,4,5,6,7,8,9,10)
 
+# Mapping Type : dict:-
 
+x = {'Name':'Ahmed','Age': 16,'City':'Karachi'}
+a = {1:'Apple',2:'Banana',3:'Kiwi' }

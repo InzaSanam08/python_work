721d0a2738ee2cbe0c1c55453066e6173a25cbb0
InzaSanam08 <inzasanam7@gmail.com>
2025-01-28 11:04:55 +0500

    python Notes

--- python_work/Data_Types/Veriables.py
+++ python_work/Data_Types/Veriables.py
@@ -1,8 +1,11 @@
 name = 'Ali'
 age = 15
 print(name,age)
+print(f'{name,age}')
 
 studentDetail: str = 'Student of midle school'
 print(studentDetail[0])
+# veriable type
 print(type(studentDetail))
+# length of studentDetail 
 print(len(studentDetail))

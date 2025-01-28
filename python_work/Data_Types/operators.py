dabfa0c3dc86d004aec486539aae2078d0a6cbac
InzaSanam08 <inzasanam7@gmail.com>
2025-01-28 12:21:53 +0500

    python Notes

---
+++ python_work/Data_Types/operators.py
@@ -0,0 +1,55 @@
+# Operators are used to perform operations on variables and values
+# Python divides the operators in the following groups:
+
+# Arithmetic operators
+# Assignment operators
+# Comparison operators
+# Logical operators
+# Identity operators
+# Membership operators
+# Bitwise operators
+
+
+# Python Arithmetic Operators
+# Arithmetic operators are used with numeric values to perform common mathematical operations:
+
+# Operator	Name	
+#    +      Addition	
+#    -  	Subtraction
+#    *  	Multiplication		
+#    /  	Division		
+#    %	    Modulus
+#   **  	Exponentiation	
+#   //	    Floor division	x 
+
+
+x = 7
+a = 3
+print(x+a)
+
+b = x+a
+print(b)
+
+
+
+
+# Python Assignment Operators
+# Assignment operators are used to assign values to variables:
+
+# Operator	Example	Same As	Try it
+#   =	
+#   +=	
+#   -=	
+#   *=	
+#   /=	
+#   %=	
+#   //=	
+#   **=	
+#   &=	
+#   |=	
+#   ^=	
+#   >>=	
+#   <<=	
+#   :=	
+# print(x)	
+
